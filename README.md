a977da14972c7604dffe5b8663ab6eeb92901776
Carl Vuorinen <carl.vuorinen@gmail.com>
2019-05-27 22:22:06 +0200

    fixed urlencoding of peer id + info hash

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# SeedSmash v 0.2 BETA
+# SeedSmash v 0.3 BETA
 
 This tool sends requests with fake upload to torrents announce.
-
-In this version it works only with one torrent in folder.
+I know this is kind of fucking with the torrent community but i was bored and pissed at my favorite tracker that they restricted new torrents just for VIP users that I started working on this so i can get VIP on tracker for free :p and it's more than 1 year and I'm still not banned *yet*
 
 Usage:
-./seedsmash.py [minutes to seed] [max speed in kB/s (max 10 000)]
+```usage: seedsmash.py [-h] --time TIME [--speed SPEED]
+  --time TIME    Seed time in minutes
+  
+optional arguments:
+  -h, --help     show this help message and exit
+  --speed SPEED  Upload speed in kBs default 1000
+  ```
 
-installing requirements: pip install -r requirements.txt
+installing requirements: `pip install -r requirements.txt`
 
-btw I'm not responsible for banned accounts, broken lives, nuclear wars etc. use this tool on your own responsibility
+#Working:
+- seeding one torrent for certain period of time - depends from announce
+
+#Not working (TODO):
+- prolonging seeding time
+- seeding multiple torrents (Multi threading)
+-
+
+
+##DISCLAIMER
+I'm not responsible for banned accounts, broken lives, nuclear wars etc. Use this tool on your own responsibility!
--- seedsmash.py
+++ seedsmash.py
@@ -1,132 +1,245 @@
 #!/usr/bin/python3
-print('######## SeedSmash 0.2 BETA by makuga01 ########\n')
+print("######## SeedSmash 0.3 BETA by makuga01 ########\n")
 try:
-    import bencodepy, bencoding, hashlib, base64, random, requests, time, sys, os, urllib.parse, codecs
+    import bencodepy, bencoding, hashlib, base64, random, requests, time, argparse, os, urllib.parse, codecs, re
 except ImportError:
-    raise ImportError('Maybe installing requirements can help, try: pip install -r requirements.txt')
+    raise ImportError(
+        "Maybe installing requirements can help, try: pip install -r requirements.txt"
+    )
 
-#decodes given hash to hex and urlencodes it
+
+def grab(string, start, end):
+    """
+    shame on me for this, borrowed code from https://github.com/s0md3v/regxy amazing guy, check him out
+    this functions grabs string betweed 2 given start and stop and raturns it
+    eg. torrent anncounce returns something like this:
+    d8:intervali7200e12:min...
+    i just need to know when to contact announce again to keep seeding for longer time
+    Using it like this grab('d8:intervali7200e12:min...', 'intervali', 'e12')  returns 7200 - time i need
+    """
+    match = re.search(r"%s[^<]*%s" % (start, end), string)
+    if match:
+        return match.group().split(start)[1][: -len(end)]
+    else:
+        return False
+
+def hex2bytes(hex_sring):
+    return codecs.decode(hex_sring, "hex")
+
 def uencode(udata):
+
+    """decodes given hash to hex and urlencodes it"""
+
     hexd = codecs.decode(udata, "hex")
-    d = {'d':hexd}
+    d = {"d": hexd}
     encoded = urllib.parse.urlencode(d)[2:]
     return encoded
 
-#Returns urlencoded info hash from torrent file
+
 def get_info_hash(tfile):
+
+    """Returns urlencoded info hash from torrent file"""
+
     objTorrentFile = open(tfile, "rb")
     decodedDict = bencoding.bdecode(objTorrentFile.read())
     info_hash = hashlib.sha1(bencoding.bencode(decodedDict[b"info"])).hexdigest()
-    enc_hash = uencode(info_hash)
-    return enc_hash
+    # enc_hash = uencode(info_hash)
+    return hex2bytes(info_hash)
 
-#Returns announce and pid in list [announce,pid]
+
 def get_announce(tfile):
+
+    """Returns announce and pid in list [announce,pid]"""
+
     meta = bencodepy.decode_from_file(tfile)
-    url = meta[b'announce'].decode()
-    ann = url.split('?')[0]
-    pid = url.split('=')[1]
+    url = meta[b"announce"].decode()
+    ann = url.split("?")[0]
+    pid = url.split("=")[1]
     return [ann, pid]
 
-#Returns urlencoded peer_id - id of torrent I'm using utorrent 2.210 (-UT2210- can be changed to whatever you want) 8 chars + 24 random chars (0123456789abcdef)
+
 def get_peer_id():
-    chars = list('0123456789abcdef')
-    p1 = ''
-    for i in range(24):
-        p1 += urllib.parse.quote_plus(random.choice(chars))
-    peer_id = '-UT2210-'+uencode(p1)
+
+    """Returns (bytes) peer_id - id
+    of torrent I'm using utorrent 2.210 (-UT2210- can be
+    changed to whatever you want) 8 chars + 24 random
+    chars (0123456789abcdef)"""
+
+    chars = list("0123456789abcdef")
+    p1 = ''.join([urllib.parse.quote_plus(random.choice(chars)) for _ in range(24)])
+    # for i in range(24):
+    #     p1 += urllib.parse.quote_plus(random.choice(chars))
+    peer_id = "-UT2210-".encode() + hex2bytes(p1)
     return peer_id
 
-#Returns key - 8 random characters (0123456789ABCDEF)
+
+#
 def get_key():
-    key = ''
-    chars = list('0123456789ABCDEF')
+
+    """Returns key - 8 random characters (0123456789ABCDEF)"""
+
+    key = ""
+    chars = list("0123456789ABCDEF")
     for i in range(8):
-        key += urllib.parse.quote_plus(random.choice(chars))
+        key += random.choice(chars)
     return key
 
-#Returns list of torrent files in current directory
+
+
 def get_files():
+
+    """Returns list of torrent files in current directory"""
+
     torrlist = []
     for file in os.listdir():
-        if file.endswith('.torrent'):
+        if file.endswith(".torrent"):
             torrlist.append(file)
     return torrlist
 
-#main function
-def seed():
-    if len(sys.argv) < 3:
-        print('Usage:\n'+sys.argv[0]+' <time in minutes> <Max up speed in kB/s(max 10 000)>\n')
-        exit(0)
 
-    seed_time = int(sys.argv[1])*60 #Time in seconds
-    print('seeding for',seed_time, 'seconds')
+def start_seed(torrfile):
+    file = torrfile
+    print(f'choosing file {file}')
 
-    upspeed = int(sys.argv[2])
-
-    #list torrent files in current dir and select first one (temporary)
-    tofiles=get_files()
-    file=tofiles[0]
-    print('choosing file',file)
-
-    #Get announce and pid
+    """Get announce and pid"""
     ann = get_announce(file)[0]
     pid = get_announce(file)[1]
 
-    #peer id and port stays constant on every torrent
-    peer_id = get_peer_id()
-    port = random.randint(10000, 65536)
-
     hash = get_info_hash(file)
-    key = get_key()
 
-    #Set user agent to utorrent 2.210
-    header={'user-agent':'uTorrent/2210(25534)'}
+    """
+    craft start url from given info.
+    """
+    start = {
+        'pid': pid,
+        'info_hash': hash,
+        'peer_id': peer_id,
+        'port': port,
+        'uploaded': 0,
+        'downloaded': 0,
+        'left': 0,
+        'corrupt': 0,
+        'key': key,
+        'event': 'started',
+        'numwant': 200,
+        'compact': 1,
+        'no_peer_id': 1
+    }
 
-    #craft start url from given info
-    start = ann+'?pid='+pid+'&info_hash='+hash+'&peer_id='+peer_id+'&port='+str(port)+'&uploaded=0&downloaded=0&left=0&corrupt=0&key='+key+'&event=started&numwant=200&compact=1&no_peer_id=1'
 
-    #send request
-    r = requests.get(start, headers=header)
-    print('\nResponse from announce:')
-    print(r.text, '\n')
-    start = time.time()
-    #generate random upload in range 0.8-1x of given up speed
-    randup = round(random.uniform(0.8,1)*seed_time*upspeed*1000)
+    # send request
+    r = requests.get(ann, params=start, headers=header)
+    print(f"Response from announce: {r.text}")
+    intervali = grab(r.text, "intervali", "e12")
+    if intervali:
+        return {"filename": torrfile, "start": time.time(), "intervali": intervali}
+    else:
 
-    #Craft stop url
-    stop = ann+'?pid='+pid+'&info_hash='+hash+'&peer_id='+peer_id+'&port='+str(port)+'&uploaded='+str(randup)+'&downloaded=0&left=0&corrupt=0&key='+key+'&event=stopped&numwant=0&compact=1&no_peer_id=1'
+        return None
 
-    #sleep for given period of time and send request, when ctrl-c is pressed - end seeding
+
+def stop_seed(torrfile, starttime):
+
+    file = torrfile
+    ann = get_announce(file)[0]
+    pid = get_announce(file)[1]
+    hash = get_info_hash(file)
+
+    # generate random upload in range 0.8-1x of given up speed
+    randup = round(random.uniform(0.8, 1) * seed_time * upspeed * 1000)
+
+    # Craft stop url
+
+    stop = {
+        'pid': pid,
+        'info_hash': hash,
+        'peer_id': peer_id,
+        'port': port,
+        'uploaded': randup,
+        'downloaded': 0,
+        'left': 0,
+        'corrupt': 0,
+        'key': key,
+        'event': 'stopped',
+        'numwant': 0,
+        'compact': 1,
+        'no_peer_id': 1
+    }
+
+    # sleep for given period of time and send request, when ctrl-c is pressed - end seeding
     try:
-        print('seeding, press ctrl-c to stop')
+        print("seeding, press ctrl-c to stop")
         time.sleep(seed_time)
     except KeyboardInterrupt:
-        print('\nShutting down SeedSmash')
-        #calculate elapsed time from start of seeding
-        elapsed_time = time.time()-start
+        print("\nShutting down SeedSmash")
+        # calculate elapsed time from start of seeding
+        elapsed_time = time.time() - starttime
 
-        #adjust upload from default time to elapsed
+        # adjust upload from default time to elapsed
         randup = randup * (elapsed_time / seed_time)
 
-        #craft another stop url with changed upload
-        stop = ann+'?pid='+pid+'&info_hash='+hash+'&peer_id='+peer_id+'&port='+str(port)+'&uploaded='+str(randup)+'&downloaded=0&left=0&corrupt=0&key='+key+'&event=stopped&numwant=0&compact=1&no_peer_id=1'
+        # craft another stop url with changed upload
 
-        #Send request
-        r = requests.get(stop, headers=header)
+        stop = {
+            'pid': pid,
+            'info_hash': hash,
+            'peer_id': peer_id,
+            'port': port,
+            'uploaded': randup,
+            'downloaded': 0,
+            'left': 0,
+            'corrupt': 0,
+            'key': key,
+            'event': 'stopped',
+            'numwant': 0,
+            'compact': 1,
+            'no_peer_id': 1
+        }
 
-        #print elapsed time + response from stop request + how much data was "uploaded"
-        print('Response from announce:')
-        print(r.text,'\n')
-        print('\nUploaded', round(randup/1000), 'kB in', round(elapsed_time),'seconds')
+        # Send request
+        r = requests.get(ann, params=stop, headers=header)
+
+        # print elapsed time + response from stop request + how much data was "uploaded"
+        print(f"Response from announce:{r.text}")
+        print(f"Uploaded {round(randup/1000)} kB in {round(elapsed_time)} seconds")
+
         exit(0)
-    #send stop request
-    print('sending stop request, don\'t ctrl-c me now pls')
-    r = requests.get(stop, headers=header)
-    elapsed_time = time.time()-start
-    #print elapsed time + response from stop request + how much data was "uploaded"
-    print('Response from announce:')
-    print(r.text,'\n')
-    print('\nUploaded', round(randup/1000), 'kB in', round(elapsed_time),'seconds')
+    # send stop request
+    print("sending stop request, don't ctrl-c me now pls")
+    r = requests.get(ann, params=stop, headers=header)
+    elapsed_time = time.time() - starttime
+    # print elapsed time + response from stop request + how much data was "uploaded"
+    print(f"Response from announce:{r.text}")
+    print(f"Uploaded {round(randup/1000)} kB in {round(elapsed_time)} seconds")
 
-seed()
+
+def seed():
+
+
+
+
+
+    test_torrent = get_files()[0]
+
+    startjson = start_seed(test_torrent)
+    stop_seed(test_torrent, startjson['start'])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--time', type=int, required=True, help='Seed time in minutes')
+    parser.add_argument('--speed', type=int, default=1000, help='Upload speed in kBs default 1000')
+    args = parser.parse_args()
+
+    # these stay constant for every torrent in one run
+    peer_id = get_peer_id()
+    port = random.randint(10000, 65536)
+    key = get_key()
+    # Set user agent to utorrent 2.210. Can be set to whatever you want but i prefer Utorrent
+    header = {"user-agent": "uTorrent/2210(25534)"}
+
+    upspeed = args.speed
+    seed_time = args.time * 60  # Get time in seconds
+
+
+
+    seed()
